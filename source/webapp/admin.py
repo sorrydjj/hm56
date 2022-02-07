b1e028a9f424bb6a1744ffbdd79a746121c15338
Ruslan <xenoncraftkg2@mail.ru>
2022-02-07 13:24:23 +0600

    создана модель данных

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from webapp.models import Category, Basket, Product
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Basket)
+admin.site.register(Product)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    names = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.names}"
+
+    class Meta:
+        db_table = "categories"
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="name")
+    descriptions = models.TextField(max_length=2000, blank=True, null=True, verbose_name="description")
+    category = models.ForeignKey("webapp.Category", on_delete=models.PROTECT, related_name="categorys")
+    count = models.IntegerField(verbose_name="counts", default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Price", null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        db_table = "products"
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+
+class Basket(models.Model):
+    products = models.ForeignKey("webapp.Product",
+                                on_delete=models.PROTECT,
+                                related_name="products"
+                                )
+    counts = models.IntegerField(verbose_name="count", default=0)
+
+    def __str__(self):
+        return f"{self.products} | {self.counts}"
+
+
+    class Meta:
+        db_table = "basket"
+        verbose_name = "Корзина"
+        verbose_name_plural = "Корзина"
